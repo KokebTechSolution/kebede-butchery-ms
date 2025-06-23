4704c2023961b1f56090d8f4dfdd087771e75cca
alki45 <alikibretmuhamed@gmail.com>
2025-06-23 10:33:57 +0300

    checkout

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+
+User = get_user_model()
 class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
