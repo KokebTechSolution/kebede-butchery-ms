202c0b75fc8a3f54e54845fbf9ed0267b4f856a7
Bereket Rezenom <bereketrezenom@gmail.com>
2025-07-31 13:58:47 +0300

    Remove apt-get commands and use psycopg 3.0.19

--- backend/build.sh
+++ backend/build.sh
@@ -2,15 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y \
-    libpq-dev \
-    gcc \
-    python3-dev \
-    libffi-dev \
-    libssl-dev
-
 pip install --upgrade pip
 pip install -r requirements.txt
 
