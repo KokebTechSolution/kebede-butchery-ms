f44ba8f7cc5abb563e471bd9a9b8121bf66d2741
Bereket Rezenom <bereketrezenom@gmail.com>
2025-08-27 15:11:15 +0300

    redeployed

--- backend/kebede_pos/settings.py
+++ backend/kebede_pos/settings.py
@@ -80,14 +80,13 @@ JWT_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # CORS must be first
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   'corsheaders.middleware.CorsMiddleware' 
 ]
 ROOT_URLCONF = 'kebede_pos.urls'
 
@@ -191,6 +190,19 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-requested-with',
 ]
+
+# CORS methods
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# CORS preflight max age
+CORS_PREFLIGHT_MAX_AGE = 86400
 
 SIMPLE_JWT = {
     'TOKEN_OBTAIN_SERIALIZER': 'kebede_pos.views.MyTokenObtainPairSerializer',
